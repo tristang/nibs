c46fbdcc7371bf78c48673b8137bdc7ed8182278
Tristan Gamilis <tristan@2suggestions.com.au>
2013-07-28 15:31:26 +1000

    Improved articles index layout

--- public_html/wp-content/themes/nibs/category.php
+++ public_html/wp-content/themes/nibs/category.php
@@ -19,8 +19,11 @@
 <?php while ( have_posts() ) : the_post(); ?>
 	<li>
 		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
+
+			<div class='post-list-item-head' style="background-image: url(<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ) ?>)">
+				<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+				<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?></time>
+			</div>
 			<?php the_excerpt(); ?>
 		</article>
 		<hr />
